4c41674c65161a5c9ba99a211fd2aedbf7bd5db9
Cristiano Araujo <crisd.silva75@gmail.com>
2025-09-08 13:52:34 -0300

    Post, author, comment

--- src/main/java/com/devsuperior/workshopmongo/config/TestConfig.java
+++ src/main/java/com/devsuperior/workshopmongo/config/TestConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.devsuperior.workshopmongo.models.entities.User;
+import com.devsuperior.workshopmongo.repositories.PostRepository;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
 
 import jakarta.annotation.PostConstruct;
@@ -18,15 +19,20 @@ public class TestConfig {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@PostConstruct
 	public void init() {
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
 	}
 }
