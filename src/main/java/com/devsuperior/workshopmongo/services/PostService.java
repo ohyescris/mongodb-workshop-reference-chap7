a57bc4172d9d657248d5fffe0c225bc78bed9b16
Cristiano Araujo <crisd.silva75@gmail.com>
2025-09-09 10:57:31 -0300

    Get Post by title, query methods

--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -1,6 +1,8 @@
 package com.devsuperior.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,6 +26,11 @@ public class PostService {
 	private Post getEntityById(String id) {
 		Optional<Post> result = repository.findById(id);
 		return result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<PostDTO> findByTitle(String text) {
+		List<Post> list = repository.findByTitleContainingIgnoreCase(text);
+		return list.stream().map(x -> new PostDTO(x)).collect(Collectors.toList());
 	}
 
 }
